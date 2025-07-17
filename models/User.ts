58253cc821315fa17116740227625976ccd13142
Er-Kapil <157969010+Er-Kapil@users.noreply.github.com>
2025-07-17 17:17:21 +0530

    First commit

---
+++ models/User.ts
@@ -0,0 +1,36 @@
+import mongoose,{Schema,model,models} from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+
+export interface IUSer {
+    email:string;
+    password:string;
+    _id:mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+
+const userSchema = new Schema<IUSer>({
+    email: {
+        type: String,
+        required: [true, "Please enter email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter password"],
+    },
+},{
+    timestamps: true,
+});
+
+userSchema.pre('save',async function(next){
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password,10)
+    }
+    next()
+})
+
+export default models.User || model<IUSer>("User", userSchema);
